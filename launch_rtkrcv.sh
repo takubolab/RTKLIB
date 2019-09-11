b12feaa26058ba79deb364414ebacb4e679375dd
Osamu Sekino <osamu.sekino@tier4.jp>
2019-09-11 13:44:55 +0900

    Rename file

--- launch_rtkrcv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Sekino
-
-cd $HOME/RTKLIB/app/rtkrcv/gcc
-
-./rtkrcv -s -o ../conf/imu_gnss_localizer.conf
---
+++ rtklib_ros_bridge_sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#MapIV Sekino
+
+cd $HOME/RTKLIB/app/rtkrcv/gcc
+
+./rtkrcv -s -o ../conf/rtklib_ros_bridge_sample.conf
